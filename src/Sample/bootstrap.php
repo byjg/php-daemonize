0d82c06017bbf4c51a25e9c2fb0fd0b3107cfbd8
João Gilberto Magalhães <joao@byjg.com.br>
2015-07-30 10:06:26 -0300

    Fix autload in sample

--- src/Sample/bootstrap.php
+++ src/Sample/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
-$autoload = realpath(__DIR__ . "/../vendor/autoload.php");
+$autoload = __DIR__ . "/../vendor/autoload.php";
 if (!file_exists($autoload))
 {
-    $autoload = realpath(__DIR__ . "/../../../autoload.php");
+    $autoload = __DIR__ . "/../../../../autoload.php";
     if (!file_exists($autoload))
     {
         throw new \Exception('Autoload not found. Did you run `composer dump-autload`?');
